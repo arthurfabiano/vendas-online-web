d82dbdb9536a476bb7033ad8068ea0883b12e653
Arthur Fabiano <arthur.masterdevelop@gmail.com>
2024-02-06 16:01:26 -0300

    feat: Initial settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['simple-import-sort', 'dist', '.eslintrc.cjs', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
